5e83743c20d515b03c6f9764a3c4d679be2149dc
GentlemanGit <ivanboltrushko@gmail.com>
2021-06-21 14:39:45 +0300

    feat: complete task

--- JS_Task1/script.js
+++ JS_Task1/script.js
@@ -1,42 +1,72 @@
 const NUMBER = document.querySelector(".number");
 const CONVERTED = document.querySelector(".converted");
-let DoubleNumber = "";
+const CHECKBOXES = document.querySelectorAll(".checkbox");
+
+let SecondNumderFirstOctet = [];
+let FirstNumderSecondOctet = [];
+let SecondNumderSecondOctet = [];
+
+function checkChecbox() {
+  CHECKBOXES.forEach((item, index) => {
+    if (item.checked == true) {
+      if (index < 4) SecondNumderFirstOctet.push(item);
+      else if (index < 11) FirstNumderSecondOctet.push(item);
+      else SecondNumderSecondOctet.push(item);
+    }
+  });
+}
+
 function checkOddEvenNumber(number) {
   if (number.value) {
-    return number.value % 2 == 0 ? "00000000" : "10000000";
+    return number.value % 2 == 0 ? "H'0" : "H'8";
   } else return "";
 }
 
-NUMBER.oninput = function () {
-  DoubleNumber = checkOddEvenNumber(NUMBER);
-  CONVERTED.value = "H'" + parseInt(DoubleNumber, 2);
-};
+function summationBinaryNumner (OctetArray) {
+  let sumNumbers = "0000";
 
+  for (let i = 0; OctetArray.length > i; i++) {
+    let current = sumNumbers.length - 1;
+    let next = OctetArray[i].value.length - 1;
+    let carry = 0;
+    let sum = "";
 
+    while (current >= 0 || next >= 0) {
+      let x = current < 0 ? 0 : +sumNumbers[current];
+      let y = next < 0 ? 0 : +OctetArray[i].value[current];
+      let val = x + y + carry;
 
+      carry = val > 1 ? 1 : 0;
+      sum = (val % 2) + sum;
 
-let addBinary = (a, b) => {
-  // handle corner cases when either input is empty
-  if (a === null || a.length === 0) return b;
-  if (b === null || b.length === 0) return a;
-
-  let i = a.length - 1;
-  let j = b.length - 1;
-  let carry = 0;
-  let sum = '';
-
-  while (i >=0 || j >= 0) {
-    let x = i < 0 ? 0 : +a[i];
-    let y = j < 0 ? 0 : +b[i];
-    let val = x + y + carry;
-
-    carry = val > 1 ? 1 : 0;
-    sum = val % 2 + sum;
-
-    i--;
-    j--;
+      current--;
+      next--;
+    }
+    sumNumbers = carry > 0 ? carry + sum : sum;
   }
-
-  return carry > 0 ? carry + sum : sum;
+  return sumNumbers;
 };
-console.log(addBinary ('10000000', '10000000'));
+
+function Output() {
+  checkChecbox();
+  let FinalyNumber = `${checkOddEvenNumber(NUMBER)}${parseInt(
+    summationBinaryNumner(SecondNumderFirstOctet),
+    2
+  )}${parseInt(summationBinaryNumner(SecondNumderSecondOctet), 2)
+    .toString(16)
+    .toUpperCase()}${parseInt(summationBinaryNumner(FirstNumderSecondOctet), 2)
+    .toString(16)
+    .toUpperCase()}`;
+  CONVERTED.value = FinalyNumber;
+  if (!NUMBER.value) {
+    CONVERTED.value = "";
+  }
+  SecondNumderFirstOctet = [];
+  FirstNumderSecondOctet = [];
+  SecondNumderSecondOctet = [];
+}
+
+CHECKBOXES.forEach((c) => {
+  c.addEventListener("click", Output);
+});
+NUMBER.oninput = Output;
